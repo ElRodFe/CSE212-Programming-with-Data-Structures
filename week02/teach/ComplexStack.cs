8443d759b747097308d742b5cd4beb2d2fe8e3f1
ElRodFe <el.rod.fe@gmail.com>
2025-05-14 16:14:28 -0300

    W02 Team Activity: Practice Stacks and Queues (finished)

--- week02/teach/ComplexStack.cs
+++ week02/teach/ComplexStack.cs
@@ -1,19 +1,32 @@
-﻿public static class ComplexStack {
-    public static bool DoSomethingComplicated(string line) {
+﻿public static class ComplexStack
+{
+
+    // Input: (a == 3 or (b == 5 and c == 6)) = True - Braces have a match
+
+    //Input: (students]i].Grade > 80 and students[i].Grade < 90) = False - 2 missing square Brackets
+
+    //Input:(robot[id + 1].Execute(.Pass() || (!robot[id * (2 + i)].Alive && stormy) || (robot[id - 1].Alive && lavaFlowing)) = False - 1 missing curly bracket
+    public static bool DoSomethingComplicated(string line)
+    {
         var stack = new Stack<char>();
-        foreach (var item in line) {
-            if (item is '(' or '[' or '{') {
+        foreach (var item in line)
+        {
+            if (item is '(' or '[' or '{')
+            {
                 stack.Push(item);
             }
-            else if (item is ')') {
+            else if (item is ')')
+            {
                 if (stack.Count == 0 || stack.Pop() != '(')
                     return false;
             }
-            else if (item is ']') {
+            else if (item is ']')
+            {
                 if (stack.Count == 0 || stack.Pop() != '[')
                     return false;
             }
-            else if (item is '}') {
+            else if (item is '}')
+            {
                 if (stack.Count == 0 || stack.Pop() != '{')
                     return false;
             }
